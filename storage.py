a74433bccbd025469057f16347656630821576fe
Eugene Shatsala Lumwaji <110747634+Lennx-gif@users.noreply.github.com>
2025-05-28 08:08:22 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -6,7 +6,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 def log_user_activity(data):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     credentials_json = os.getenv("tg_Bot_JSON")
-    creds_dict = json.load(StringIO(credentials_json))
+    creds_dict = json.load(StringIO(tg_Bot_json))
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
     
