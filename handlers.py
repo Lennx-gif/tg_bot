332ec72f98d2a3119df6c0a06867c83adfcd260d
Eugene Shatsala Lumwaji <110747634+Lennx-gif@users.noreply.github.com>
2025-05-28 07:48:26 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,6 +1,10 @@
 # handlers.py
 from turtle import update
 
+def setup_handlers(app):
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("setphone", set_phone))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
 async def course_not_found(update: update):
     await update.message.reply_text("Sorry, course not found. Please describe it, and we'll try to find it manually.")
